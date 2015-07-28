6a2ead9b90ba4486e19e0392d516242250b7b0c2
qvarker <samuel.ureta@gmail.com>
2015-07-28 14:52:00 +0200

    Upgraded Makefile and added a new install.sh script

---
+++ tools/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd ~/DimaNET-master/tools
+make upgrade-system install-deps
+cd RTL8188-hostapd-2.0/hostapd
+make 
+sudo make install
+echo " "
+echo "Configurando servicios al inicio..."
+echo " "
+update-rc.d isc-dhcp-server remove
+update-rc.d isc-dhcp-server defaults
+update-rc.d hostapd remove
+update-rc.d hostapd defaults
+cd ~/DimaNET-master/tools
+make set-configs image
